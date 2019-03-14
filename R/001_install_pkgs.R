e9da37dc36eddd4ae391aa7e52f59bdc2dc6022f
pfistfl <pfistererf@googlemail.com>
2019-03-14 11:43:28 +0100

    change path

--- R/001_install_pkgs.R
+++ R/001_install_pkgs.R
@@ -1,5 +1,5 @@
-pkg_dir  = "/home/flo/Documents/repos/r_pkgs_randombotng" # Download packages to
-repo_dir = "/home/flo/Documents/repos/r_drat"             # Cran-like repo folder
+# pkg_dir  = "/home/flo/Documents/repos/r_pkgs_randombotng" # Download packages
+repo_dir = "/naslx/projects/pr74ze/di25pic2/randombot_files/r_drat" # Cran-like repo folder
 
 
 pkgs = c("e1071", "xgboost", "ranger", "rpart", "LiblineaR", "glmnet", "mlr",
@@ -10,25 +10,25 @@ pkgs = c("e1071", "xgboost", "ranger", "rpart", "LiblineaR", "glmnet", "mlr",
 ## REPO_DIR can now act like a offline CRAN Mirror.
 # install.packages("drat")
 # require(drat)
-down_pkg_deps = function(pkg, destdir,
-  which = c("Depends", "Imports", "LinkingTo"),
-  inc.pkg = TRUE, repos = getOption("repos")) {
-  stopifnot(require("tools")) ## load tools
-  ap = available.packages(repos = repos) ## takes a minute on first use
-  ## get dependencies for pkg recursively through all dependencies
-  deps = package_dependencies(pkg, db = ap, which = which, recursive = TRUE)
-  ## the next line can generate warnings; I think these are harmless
-  ## returns the Priority field. `NA` indicates not Base or Recommended
-  pri <- sapply(deps[[1]], packageDescription, fields = "Priority")
-  ## filter out Base & Recommended pkgs - we want the `NA` entries
-  deps <- deps[[1]][is.na(pri)]
-  ## install pkg too?
-  if (inc.pkg) {
-    deps = c(pkg, deps)
-  }
-  download.packages(deps, destdir = destdir)
-  deps ## return dependencies
-}
+# down_pkg_deps = function(pkg, destdir,
+#   which = c("Depends", "Imports", "LinkingTo"),
+#   inc.pkg = TRUE, repos = getOption("repos")) {
+#   stopifnot(require("tools")) ## load tools
+#   ap = available.packages(repos = repos) ## takes a minute on first use
+#   ## get dependencies for pkg recursively through all dependencies
+#   deps = package_dependencies(pkg, db = ap, which = which, recursive = TRUE)
+#   ## the next line can generate warnings; I think these are harmless
+#   ## returns the Priority field. `NA` indicates not Base or Recommended
+#   pri <- sapply(deps[[1]], packageDescription, fields = "Priority")
+#   ## filter out Base & Recommended pkgs - we want the `NA` entries
+#   deps <- deps[[1]][is.na(pri)]
+#   ## install pkg too?
+#   if (inc.pkg) {
+#     deps = c(pkg, deps)
+#   }
+#   download.packages(deps, destdir = destdir)
+#   deps ## return dependencies
+# }
 # down_pkg_deps("mlrCPO", destdir = pkg_dir)
 # pkgs = list.files(pkg_dir, full.names = TRUE)
 # sapply(pkgs, function(x) {drat::insertPackage(x, repo_dir)})
