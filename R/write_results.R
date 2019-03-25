2aa1a07dce7c91963c4cf8f1ad9654a33ea1fd5b
mb706 <mlr.developer@mb706.com>
2019-03-25 13:20:32 +0100

    shorter filenames

--- R/write_results.R
+++ R/write_results.R
@@ -3,7 +3,7 @@
 rbn.writeResult <- function(result, task, learner, arg) {
   rbn.setOutputDir()
 
-  file <- file.path(rbn.getSetting("OUTPUTDIR"), sprintf("RESULT_%s_%s_%s.rds.gz", task, learner, arg))
+  file <- file.path(rbn.getSetting("OUTPUTDIR"), sprintf("RESULT_%s_%s_%s.rds.gz", task, learner, substr(digest::digest(arg), 1, 16)))
 
   saveRDS(result, file = file, version = 2, compress = "gzip")
 }
