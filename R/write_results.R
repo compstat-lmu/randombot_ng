3352bbc0eb6c3562f0d6f0cbba5902bfbce12446
mb706 <mlr.developer@mb706.com>
2019-03-26 13:27:56 +0100

    tell us what file you're writing to

--- R/write_results.R
+++ R/write_results.R
@@ -5,6 +5,9 @@ rbn.writeResult <- function(result, task, learner, arg) {
 
   file <- file.path(rbn.getSetting("OUTPUTDIR"), sprintf("RESULT_%s_%s_%s.rds.gz", task, learner, substr(digest::digest(arg), 1, 16)))
 
+  catf("Writing to %s", file)
+
   saveRDS(result, file = file, version = 2, compress = "gzip")
+  catf("Done Writing")
 }
 
