83b2e41814b5f88c13aaca6f559b51b60bb6da0a
pfistfl <pfistererf@googlemail.com>
2019-03-26 16:34:09 +0100

    Fix hnsw learner and remove stupid printer

--- input/learners/RLearner_classif_rcpphnsw.R
+++ input/learners/RLearner_classif_rcpphnsw.R
@@ -4,11 +4,11 @@ makeRLearner.classif.RcppHNSW = function() {
     cl = "classif.RcppHNSW",
     package = "RcppHNSW",
     par.set = makeParamSet(
-      makeIntegerLearnerParam(id = "k", lower = 1L, default = 1L, upper = 50L, when = "predict"),
+      makeIntegerLearnerParam(id = "k", lower = 1L, default = 1L, when = "predict", upper = 50L),
       makeDiscreteLearnerParam(id = "distance", values = c("euclidean", "l2", "cosine", "ip"), default = "euclidean"),
-      makeIntegerLearnerParam(id = "M", lower = 2, upper = 1000, default = 16),
-      makeIntegerLearnerParam(id = "ef", lower = 1, upper = 10^5, default = 10, when = "predict"),
-      makeIntegerLearnerParam(id = "ef_construction", lower = 1, upper = 10^5, default = 200),
+      makeIntegerLearnerParam(id = "M", lower = 2, upper = Inf, default = 16),
+      makeIntegerLearnerParam(id = "ef", lower = 1, upper = Inf, default = 10, when = "predict"),
+      makeIntegerLearnerParam(id = "ef_construction", lower = 1, upper = Inf, default = 200),
       makeLogicalLearnerParam(id = "verbose", default = FALSE, tunable = FALSE)
     ),
     par.vals = list(k = 1, M = 16, ef_construction = 200),
@@ -32,7 +32,6 @@ predictLearner.classif.RcppHNSW = function(.learner, .model, .newdata, ...) {
   nns = RcppHNSW::hnsw_search(as.matrix(.newdata), .model$learner.model$ann,
     ...)
   tgt = .model$learner.model$target
-  print(dim(nns$idx))
   # Convert to probability matrix
   if (.learner$predict.type == "prob") {
     d = do.call("rbind", lapply(seq_len(nrow(nns$idx)), function(i) {
@@ -40,10 +39,10 @@ predictLearner.classif.RcppHNSW = function(.learner, .model, .newdata, ...) {
       levels = levels(tgt))))
     }))
   } else {
-    d = sapply(seq_len(nrow(nns$idx)), function(i) {
+    d = as.factor(sapply(seq_len(nrow(nns$idx)), function(i) {
       d = tgt[nns$idx[i,]]
       names(which.max(table(d)))
-    })
+    }))
   }
   return(d)
 }
