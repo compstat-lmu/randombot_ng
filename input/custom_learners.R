241bf11600260e2c5d8746c112828af0b7c0710f
mb706 <mlr.developer@mb706.com>
2019-03-27 00:56:26 +0100

    fixing some CPO issues

--- input/custom_learners.R
+++ input/custom_learners.R
@@ -4,14 +4,14 @@ lapply(dir("learners", "\\.R$", full.names = TRUE), source, chdir = TRUE)
 rbn.registerLearner("MODIFIER", function(lrn) {
   cpoFixFactors() %>>%
     cpoCbind(
+        cpoImputeConstant("__MISSING__", affect.type = c("factor", "ordered")) %>>%
         cpoMultiplex(id = "num.impute",
           list(
               cpoImputeMean(affect.type = "numeric"),
               cpoImputeMedian(affect.type = "numeric"),
               cpoImputeHist(affect.type = "numeric")),
           selected.cpo = "impute.hist"),
-        MISSING = cpoMissingIndicators(affect.type = "numeric")) %>>%
-    cpoImputeConstant("__MISSING__", affect.type = c("factor", "ordered")) %>>%
+        MISSING = cpoSelect(type = "numeric") %>>% cpoMissingIndicators()) %>>%
     cpoMaxFact(32) %>>%
     cpoDropConstants(abs.tol = 0) %>>% lrn
 })
@@ -21,7 +21,7 @@ xgboost.constructor <- function() {
   lrn <- makeLearner("classif.xgboost")
   lrn$par.set$pars$nrounds$upper <- 6000
   # add dummy encode, xgboost can't handle factors otherwise
-  cpoDummyEncode(TRUE) %>>% lrn
+  cpoDummyEncode(reference.cat = TRUE, infixdot = TRUE) %>>% lrn
 }
 
 rbn.registerLearner("classif.xgboost.gblinear", xgboost.constructor)
@@ -34,5 +34,5 @@ rbn.registerLearner("classif.svm.radial", function() {
 
 rbn.registerLearner("classif.RcppHNSW", function() {
   lrn <- makeLearner("classif.RcppHNSW")
-  cpoDummyEncode(TRUE) %>>% lrn
+  cpoDummyEncode(reference.cat = TRUE, infixdot = TRUE) %>>% lrn
 })
