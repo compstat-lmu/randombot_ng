1af259d2b04cce36509ff2d9c451dce31cd35d58
mb706 <mlr.developer@mb706.com>
2019-03-29 20:56:07 +0100

    sampling learner with as little variance as I can think

--- scheduling/sample_learners.R
+++ scheduling/sample_learners.R
@@ -30,8 +30,37 @@ proptable$whole <- floor(proptable$x * numlearners)
 
 proptable$given <- 0
 
+## We want to give the learners according to the proportion specified.
+# If the proportions times the number of learners lead to an expected
+# number of learners greater than one, we return those learners until
+# the remaining expected learners are only fractional.
 for (iter in seq_len(sum(proptable$whole))) {
   togive <- which.max(proptable$x * iter - proptable$given)
   cat(paste0(proptable$learner[togive], "\n"))
   proptable$given[togive] <- proptable$given[togive] + 1
+}
+
+stopifnot(all(proptable$given == proptable$whole))
+
+proptable$x <- proptable$x * numlearners - proptable$given
+
+stopifnot(all(proptable$x <= 1 & proptable$x >= 0))
+
+remaining <- numlearners - sum(proptable$whole)
+
+while (remaining) {
+  if (remaining >= nrow(proptable)) {
+    cat(paste0(proptable$learner[order(proptable$x, decreasing = TRUE)], "\n"), sep = "")
+    break
+  }
+  consider <- which.max(proptable$x)
+  if (runif(1) <= proptable$x[consider]) {
+    cat(paste0(proptable$learner[consider], "\n"))
+    proptable$x <- proptable$x * (sum(proptable$x) - 1) / sum(proptable$x[-consider])
+    remaining <- remaining - 1
+  } else {
+    proptable$x <- proptable$x * sum(proptable$x) / sum(proptable$x[-consider])
+  }
+  proptable <- proptable[-consider, ]
+  proptable$x <- pmax(proptable$x, 0)
 }
