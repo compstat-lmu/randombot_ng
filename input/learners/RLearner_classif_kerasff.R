baf3c1e11781e4080e2e5661749fcc5a632f962a
mb706 <mlr.developer@mb706.com>
2019-03-31 19:17:26 +0200

    explicit type conversion for python II

--- input/learners/RLearner_classif_kerasff.R
+++ input/learners/RLearner_classif_kerasff.R
@@ -84,7 +84,7 @@ trainLearner.classif.kerasff  = function(.learner, .task, .subset, .weights = NU
 
   if(!is.null(init_seed)) {
     npr = reticulate::import("numpy.random")
-    npr$seed(init_seed)
+    npr$seed(as.integer(init_seed))
     K$tf$set_random_seed(as.integer(init_seed))
   }
 
