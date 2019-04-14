380a8e5b0fc54ab78b459a58258c98dca62881da
mb706 <mlr.developer@mb706.com>
2019-04-14 19:14:32 +0200

    some small fixes, using the correct SLURM env vars

--- scheduling/sbatch.cmd
+++ scheduling/sbatch.cmd
@@ -44,7 +44,7 @@ check_env DATADIR ONEOFF STRESSTEST STARTSEED DRAINPROCS REDISPORT
 if [ -f REDISINFO ] ; then rm REDISINFO || exit 102 ; fi
 echo "[MAIN]: Starting Redis"
 srun --unbuffered --export=ALL --mem="$SLURM_MEM_PER_NODE" --nodes=1 --ntasks=1 \
-     --nodelist="$SLURM_NODENAME" \
+     --nodelist="$SLURMD_NODENAME" \
      "${SCRIPTDIR}/runredis.sh" 2>&1 | \
     sed -u "s'^'[REDIS]: '" | \
     grep --line-buffered '^' &
@@ -75,7 +75,7 @@ mkdir RESULTS
 # - we want to round UP, but bash arithmetic rounds down, so we do
 #   (DRAINPROCS + [slots per node] - 1)  / [slots per node]
 DRAINNODES=$(((DRAINPROCS + (SLURM_MEM_PER_NODE / 2048) - 1) / (SLURM_MEM_PER_NODE / 2048)))
-DNLIST="$(echo "$SLURM_NODENAME" | \
+DNLIST="$(echo "$SLURMD_NODENAME" | \
 	     cat - <(scontrol show hostnames "$SLURM_JOB_NODELIST") | \
 	     sort | uniq -u | head -n "$DRAINNODES" | tr $'\n' ',')"
 echo "[MAIN]: Launching $DRAINPROCS drain processes on $DRAINNODES nodes"
@@ -89,7 +89,7 @@ echo "[MAIN]: Drain processes up."
 
 echo "[MAIN]: Calculating job step node assignment. This may take a minute or two."
 if [ -e STEPNODES ] ; then rm -r STEPNODES || exit 103 ; fi
-cat <(echo "$SLURM_NODENAME") \
+cat <(echo "$SLURMD_NODENAME") \
     <(echo "$DNLIST" | tr ',' $'\n') \
     <(scontrol show hostnames "$SLURM_JOB_NODELIST") | sort | uniq -u | \
     "${SCRIPTDIR}/assignJobSteps.R"
@@ -105,11 +105,15 @@ while read data learner memcosts ntasks ; do
 # We therefore need to make sure these parameters are not set. They are not on
 # Supermuc NG, so this should be fine.
     echo "[MAIN]: Creating $ntasks tasks working on $data with ${learner}, memcost: ${memcosts}M"
-    srun --unbuffered --export=ALL --exclusive \
+    ( srun --unbuffered --export=ALL --exclusive \
 	 --mem-per-cpu="${memcosts}M" --ntasks="$ntasks" \
 	 --nodelist="STEPNODES/${data}_${learner}.nodes" \
-	 /bin/sh -c "${SCRIPTDIR}/runscript.sh \"${data}\" \"${learner}\" \"${STARTSEED}\" \"${ONEOFF}\" \"${STRESSTEST}\" 2>&1 | sed -u \"s'^'[${task},${learner},\${SLURM_LOCALID}]: '\"" \
-	grep --line-buffered '^' &
+	 --nodes="1-${SLURM_JOB_NUM_NODES}" \
+	 --immediate \
+    	 /bin/sh -c "${SCRIPTDIR}/runscript.sh \"${data}\" \"${learner}\" \"${STARTSEED}\" \"${ONEOFF}\" \"${STRESSTEST}\" 2>&1 | sed -u \"s'^'[\${SLURM_PROCID}]: '\"" 2>&1 | \
+	  sed -u "s'^'[${data},${learner}]'"
+      echo "[${data},${learner}]: SRUN returned with status $?"
+    )&
 done <STEPNODES/STEPS
 
 wait
